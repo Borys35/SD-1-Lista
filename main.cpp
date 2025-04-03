e3eca2103db3c754c657a17ebed381da3d9991e3
Borys35 <boryskac10@gmail.com>
2025-04-03 16:03:53 +0200

    add generating and copying lists

--- main.cpp
+++ main.cpp
@@ -3,30 +3,45 @@
 #include "ArrayList.hpp"
 #include "DoublyLinkedList.hpp"
 
+void fillArray(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 1001;
+    }
+}
+
+template<typename Func>
+void arrayToList(int n, int arr[], const Func& push) {
+    for (int i = 0; i < n; i++) {
+        push(arr[i]);
+    }
+}
+
 // C++20
 int main() {
+    srand(time(nullptr));
+
     std::cout<<"Hello\n";
     DoublyLinkedList linkedList = DoublyLinkedList<int>();
     ArrayList arrayList = ArrayList<int>();
 
-    //linkedList.push_back(5);
-
-    int n = 10;
-    for (int i = 1; i < n + 1; i++) {
-        linkedList.push_front(i);
-    }
-    linkedList.remove_back();
-    linkedList.remove_front();
-    linkedList.insert(1000, 2);
-    linkedList.remove(3);
+    int size = 10;
+    int *baseArray = new int[size];
+    fillArray(baseArray, size);
+    arrayToList(size, baseArray, [&](int value) { linkedList.push_back(value); });
+    arrayToList(size, baseArray, [&](int value) { arrayList.push_back(value); });
+    delete [] baseArray;
 
     std::cout<<"[ ";
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 10; i++) {
         std::cout<<linkedList.get(i)<<", ";
     }
     std::cout<<"]\n";
 
-    // std::cout<<linkedList.get(10);
+    std::cout<<"[ ";
+    for (int i = 0; i < 10; i++) {
+        std::cout<<arrayList.get(i)<<", ";
+    }
+    std::cout<<"]\n";
 
     return 0;
 }
