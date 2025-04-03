d369ca4dffce9c6c2d163b00c6dab49980eff1af
Borys35 <boryskac10@gmail.com>
2025-04-03 18:32:08 +0200

    add todo in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 typedef std::chrono::high_resolution_clock Clock;
 
 // C++20
+// TODO: zaimplementowac SinglyLinkedList
+// TODO: rozszerzyc testy o wszystkie operacje i wszystkie typy list
+// TODO: stworzyc interaktywne menu do wyboru dzialan (testy, pojedyncze akcje itd.)
 int main() {
     srand(0);
 
